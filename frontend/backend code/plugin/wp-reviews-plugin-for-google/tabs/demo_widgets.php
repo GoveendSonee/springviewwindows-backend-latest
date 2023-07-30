6ed4155574d77c5fcc8207b9dbe5041b5c5cacc1
Govind Soni <govind.soni@exaltaretech.com>
2023-07-30 09:41:23 +0530

    Plugin Code Backend COde

---
+++ frontend/backend code/plugin/wp-reviews-plugin-for-google/tabs/demo_widgets.php
@@ -0,0 +1,65 @@
+<div class="ti-preview-boxes-container">
+<div class="ti-full-width">
+<div class="ti-box ti-preview-boxes">
+<div class="ti-box-header">
+<span class="ti-header-layout-text">
+<?php echo TrustindexPlugin_google::___('Example Widget'); ?>:
+<strong><?php echo esc_html(TrustindexPlugin_google::___(TrustindexPlugin_google::$widget_templates['templates'][4]['name'])); ?></strong>
+ (<?php echo esc_html(TrustindexPlugin_google::___(TrustindexPlugin_google::$widget_styles['light-background']['name'])); ?>)
+</span>
+</div>
+<div class="preview"><?php echo $trustindex_pm_google->get_trustindex_widget('2d9bf9019f8d93ad1430e9135'); ?></div>
+</div>
+</div>
+<?php
+$demo_list = [
+5 => 'light-minimal',
+36 => 'drop-shadow',
+34 => 'ligth-border',
+13 => 'dark-background',
+15 => 'drop-shadow',
+37 => 'ligth-border',
+33 => 'light-minimal',
+16 => 'drop-shadow-large',
+31 => 'soft',
+54 => 'light-background-large',
+6 => 'light-background-large',
+8 => 'light-background-large',
+27 => 'ligth-border-3d-large',
+26 => 'ligth-border',
+29 => 'drop-shadow',
+30 => 'dark-background',
+60 => 'light-background-large',
+25 => 'light-background-large',
+32 => 'dark-background',
+22 => 'light-background-large',
+23 => 'ligth-border',
+55 => 'light-minimal',
+11 => 'drop-shadow-large',
+12 => 'light-minimal'
+];
+foreach($demo_list as $layout => $style): ?>
+<?php
+$template = TrustindexPlugin_google::$widget_templates['templates'][ $layout ];
+$class_name = 'ti-full-width';
+if(in_array($template['type'], [ 'badge', 'button', 'floating', 'popup', 'sidebar' ]))
+{
+$class_name = 'ti-half-width';
+}
+?>
+<div class="<?php echo esc_attr($class_name); ?>">
+<div class="ti-box ti-preview-boxes" data-layout-id="<?php echo esc_attr($layout); ?>" data-set-id="<?php echo esc_attr($style); ?>">
+<div class="ti-box-header">
+<span class="ti-header-layout-text">
+<?php echo TrustindexPlugin_google::___('Example Widget'); ?>:
+<strong><?php echo esc_html(TrustindexPlugin_google::___($template['name'])); ?></strong>
+ (<?php echo esc_html(TrustindexPlugin_google::___(TrustindexPlugin_google::$widget_styles[$style]['name'])); ?>)
+</span>
+</div>
+<div class="preview">
+<?php echo $trustindex_pm_google->get_noreg_list_reviews(null, true, $layout, $style, true, true, true); ?>
+</div>
+</div>
+</div>
+<?php endforeach; ?>
+</div>
